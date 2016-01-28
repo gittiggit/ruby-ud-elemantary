03867b8b445c3ff1e2dfe2047038c0a5246da286
gittiggit <contactwithmahmud@gmail.com>
2016-01-28 10:43:27 +0600

    After have some experiment with array

---
+++ Array_index_iterate/array0.rb
@@ -0,0 +1,14 @@
+# The Book of Ruby - http://www.sapphiresteel.com
+
+def array_length( anArray )
+  return anArray.length
+end
+
+a0 = [1,2,3,4,5]
+a1 = [1,'two', 3.0, array_length( a0 ) ]
+puts "In a0 index 5 position: #{a0[5]}"
+puts a0[4]
+puts a0
+# p( a0 )
+p( a1 )
+puts( "Item index #0 of a1 is #{a1[0]}, item #3 is #{a1[3]}" )
